53730b11eb1593663dfe11d7809c03d270ef6985
camemba <camemba72@gmail.com>
2023-01-06 21:19:54 +0100

    feat : standard output added

--- Scenario.cpp
+++ Scenario.cpp
@@ -4,14 +4,15 @@
 
 #include "Scenario.h"
 
-Scenario::Scenario() : apport_initial(),apport_hebdomadaire({}){}
+Scenario::Scenario() : apport_initial(),apport_hebdomadaire(){}
 
 
 std::ostream& operator<<(std::ostream& os, const Scenario& s)
 {
-    os << "apport_initial : " << s.apport_initial << std::endl;
-    //TODO faire un iterateur pour les apports hebdo
-    //os << "apport_hebdo : " << s.apport_hebdomadaire << std::endl;
-
+    os << "{ apport_initial : " << s.apport_initial <<std::endl;
+    os <<" apport hebdo : | ";
+    for(auto apport : s.apport_hebdomadaire)
+        os << apport<<" | ";
+    os<<"}";
     return os;
 }
--- Scenario.h
+++ Scenario.h
@@ -5,7 +5,9 @@
 #ifndef PRD_22_PRODUCTION_AGRICOLE_SCENARIO_H
 #define PRD_22_PRODUCTION_AGRICOLE_SCENARIO_H
 #include <vector>
-#include <iostream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 
 class Scenario {
 public:
@@ -13,8 +15,15 @@ public:
 
 
 public:
-    float apport_initial; //apport en eau au départ
+    float apport_initial; //apport en eau au depart
     std::vector<float> apport_hebdomadaire; //apport en eau pour chaque semaine
+
+    /*
+* Macro pour "JSONiser" la classe
+* TYPE_INTRUSIVE permet d'acceder e des attributs prives
+* Chaque sous-classe utilisee doit etre JSONisable elle aussi
+*/
+    NLOHMANN_DEFINE_TYPE_INTRUSIVE(Scenario, apport_initial, apport_hebdomadaire)
 };
 std::ostream& operator<<(std::ostream& os, const Scenario& s);
 
