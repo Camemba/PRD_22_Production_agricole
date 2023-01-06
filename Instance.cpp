2e5d58fbda9fce2fd43849eb8a41e0d5d70ae8d3
camemba <camemba72@gmail.com>
2023-01-06 21:24:55 +0100

    feat : conversion cultures & scenarios field

--- Instance.cpp
+++ Instance.cpp
@@ -16,10 +16,17 @@ Instance::Instance() :id(),nb_jobs(),horizon(),nb_scenarios(),
     js.at("nb_hectars").get_to(instance.nb_hectars);
     js.at("GESMAX").get_to(instance.GESMAX);
 
-    //TODO transfomer data en vector<culture>
-    auto data = js.at("cultures");
-    for (json culture : data){
-        //std::cout << culture;
+    auto data_c = js.at("cultures");
+    for (json json_culture : data_c){
+        Culture culture = Culture();
+        nlohmann::from_json(json_culture,culture);
+        instance.cultures.push_back(culture);
+    }
+    auto data_s = js.at("scenarios");
+    for (json json_scenario : data_s){
+        Scenario scenario = Scenario();
+        nlohmann::from_json(json_scenario,scenario);
+        instance.scenarios.push_back(scenario);
     }
 
 
@@ -32,5 +39,11 @@ std::ostream& operator<<(std::ostream& os, const Instance& instance)
     os << "nb_scenarios : " << instance.nb_scenarios << std::endl;
     os << "nb_hectars : " << instance.nb_hectars << std::endl;
     os << "GESMAX : " << instance.GESMAX << std::endl;
+    os << "cultures : "<<std::endl;
+    for (auto c : instance.cultures)
+        os <<c <<std::endl;
+    os << "scenarios : "<<std::endl;
+    for (auto s : instance.scenarios)
+        os << s <<std::endl;
     return os;
 }
