425c49923003f14a3c037daa58a5734ba13ff89d
BastienCamembert <bastien.camembert@etu.univ-tours.fr>
2023-02-09 10:48:07 +0100

    feat : Launch heuristic on multiple instances

--- main.cpp
+++ main.cpp
@@ -1,31 +1,58 @@
 #include <iostream>
 #include <fstream>
 #include "Instance.h"
+#include "Solver.h"
 
 Instance buildInstance(std::string jsonFilePath)
 {
     std::ifstream file(jsonFilePath);
     json data = json::parse(file);
     Instance instance = Instance();
-    Instance::from_json(data,instance);
+    Instance::BuildFromJson(data, instance);
     return instance;
 }
 void exportInstance(std::string jsonFilePath, Instance instance)
 {
-    json json1;
-    nlohmann::to_json(json1,instance);
-    std::cout << json1 << std::endl;
+    json j;
+    nlohmann::to_json(j,instance);
+    std::cout << j << std::endl;
     std::ofstream myfile;
     myfile.open (jsonFilePath);
-    myfile << json1;
+    myfile << j;
     myfile.close();
 }
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::string jsonFile = "Instances\\generatedInstance1.json";
-
+void simpleTest(){
+    //std::string jsonFile = "Instances\\instance1.json";
+    std::string jsonFile = "Stat\\generated_test_instance0.json";
     Instance instance = buildInstance(jsonFile);
-    std::cout<<instance;
+    Solver solver = Solver(instance);
+    Solution sol = solver.Heuristique1();
+    std::cout<<sol<<std::endl;
+    std::cout<<"Resultion time : "<<sol.duration<<" ms"<<std::endl;
+}
+void makeStat(){
+    std::fstream outfile;
+    Instance instance;
+    Solution sol;
+    std::string workingDirectory = "Stat\\generated_test_instance";
+    std::string jsonFile;
+
+    int i;
+    int cumulate = 0;
+    for(i=0; i<90;i++){
+        jsonFile = workingDirectory + std::to_string(i) +".json";
+        instance = buildInstance(jsonFile);
+        Solver solver = Solver(instance);
+        sol = solver.Heuristique1();
+        outfile.open("result.txt", std::fstream::app); // append instead of overwrite
+        outfile << jsonFile <<";"<<sol.score<<";"<<sol.duration<<";"<<std::endl;
+        outfile.close();
+        cumulate+=sol.duration;
+    }
+    std::cout<<"Total : "<<cumulate<<" ms"<<std::endl;
+}
+int main(int argc, char *argv[]) {
+    simpleTest();
+    makeStat();
     return 0;
 }
